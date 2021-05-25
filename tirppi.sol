1c6e41435632fffeb659dfcf1a7e709d6a885a03
alex popescu <shlootz@gmail.com>
2021-05-25 10:43:40 +0300

    Test truffle
    Test truffle

--- tirppi.sol
+++ tirppi.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 /**
- * The iCoin is ERC20 contract does this and that...and the first commit for Truffle test
+ * The iCoin is ERC20 contract does this and that...
  */
 contract iCoin is ERC20 {
-  constructor(uint256 initialSupply) public ERC20("iCoin", "ICOIN") {
+  constructor(uint256 initialSupply) public ERC20("trippi", "TRIP") {
     _mint(msg.sender, initialSupply);
   }
 }
